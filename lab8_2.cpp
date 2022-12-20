2a138ae4c88a41ac219991d1256e92e9ff838449
godmonera <borwonpak_du@cmu.ac.th>
2022-12-20 16:12:37 +0700

    finished

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -16,3 +16,33 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name,movie , days , lastS , id;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    getline(cin,name) ;
+    cout << "?????: Fahsai: Wow!!! "<<name<<" is a really cool name.\n";
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << name <<": ";
+    cin >> id ;
+    cin.ignore();
+    cout << "Fahsai: I think you may be GEAR "<<stoi(id.substr(0,2)) - 12<<". I have a free movie ticket for you.\n";
+    cout << "Fahsai: Let's go to the cinema together!!!\n";
+    cout << "Fahsai: What movie do you want to watch?\n";
+    cout << name <<": ";
+    getline(cin,movie) ;
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    cout << name <<": ";
+    getline(cin,days) ;
+    cout << "Fahsai: "<< days <<"....that is OK!!! I'm looking forward to watching "<<movie<<" with you.\n" ;
+    cout << name <<": ";
+    getline(cin,lastS) ;
+    cout << "Fahsai: 555+ see you "<<days<<". Bye Bye \\(^ ^)/";
+
+
+    return 0;
+}
